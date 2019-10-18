06d829088ad7ee823b8c17df1862ce491cad050d
squashgray <squashgray@gmail.com>
2019-10-18 09:28:17 -0700

    prototypes

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -98,12 +98,13 @@ graduates.forEach((items) => {
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
-for (let i = 0; i < graduates.length; i++){                              
-  if (graduates[i].university == "Uni") {  
-      unisWithUni.push(graduates[i])
-  }
-};
+const unisWithUni = graduates.map(items => items["university"]).filter(items => items.includes("Uni"));
+
+// for (let i = 0; i < graduates.length; i++){                              
+//   if (graduates[i].university === "Uni") {  
+//       unisWithUni.push(graduates[i])
+//   }
+// };
 
 console.log(unisWithUni);
 
